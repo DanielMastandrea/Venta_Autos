c49015c306584be7cf1e392be460d7addba4c5a7
Angie Galvan <agalvanbeninati@gmail.com>
2022-08-07 09:28:25 -0300

    Busqueda (no funciona)

--- VENTA_DE_AUTOS/Productos/urls.py
+++ VENTA_DE_AUTOS/Productos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Productos.views import list_cars, create_car
+from Productos.views import list_cars, create_car, search_products
 
 
 urlpatterns = [
     path('list-cars/', list_cars, name='car_list'),
     path('create-car/', create_car, name='create_car'),
+    path('search-products/', search_products, name ='search_products')
 ]
--- VENTA_DE_AUTOS/Productos/views.py
+++ VENTA_DE_AUTOS/Productos/views.py
@@ -1,4 +1,3 @@
-from multiprocessing import context
 from django.shortcuts import render
 from Productos.models import Autos
 
@@ -19,3 +18,9 @@ def list_cars(request):
         'autos': autos
     }
     return render(request, 'Productos/car_list.html', context=context)
+
+def search_products(request):
+    search = request.GET['search']
+    products = Autos.objects.filter(name__icontains=search)
+    context = {'products':products}
+    return render(request, 'Productos/search_products.html', context=context)
